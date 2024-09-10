17a3dbacfcdece1a9415f11cc296e1cfe3916396
ayoub <ayoubbenomar1230@gmail.com>
2024-09-10 15:15:46 +0100

    condition

--- SAS1/conditions/conditions10.c
+++ SAS1/conditions/conditions10.c
@@ -2,46 +2,46 @@
 #include <stdlib.h>
 
 int main(){
-        int j,m,an;
+        int d,m,y;
         printf("entrer le date sous form de 10/09/2024: ");
-        scanf("%d/%d/%d", &j,&m,&an);
+        scanf("%d/%d/%d", &d,&m,&y);
         switch(m){
                 case 1:
-                        printf("%d-Janvier-%d", j,an);
+                        printf("%d-Janvier-%d", d,y);
                         break;
                 case 2:
-                        printf("%d-Fevrier-%d", j,an);
+                        printf("%d-Fevrier-%d", d,y);
                         break;
                 case 3:
-                        printf("%d-Mars-%d", j,an);
+                        printf("%d-Mars-%d", d,y);
                         break;
                 case 4:
-                        printf("%d-April-%d", j,an);
+                        printf("%d-April-%d", d,y);
                         break;
                 case 5:
-                        printf("%d-May-%d", j,an);
+                        printf("%d-May-%d", d,y);
                         break;
                 case 6:
-                        printf("%d-Juin-%d", j,an);
+                        printf("%d-Juin-%d", d,y);
                         break;
                 case 7:
-                        printf("%d-Juillet-%d", j,an);
+                        printf("%d-Juillet-%d", d,y);
                         break;
                 case 8:
-                        printf("%d-Aout-%d", j,an);
+                        printf("%d-Aout-%d", d,y);
                         break;
                 case 9:
-                        printf("%d-Septembre-%d", j,an);
+                        printf("%d-Septembre-%d", d,y);
                         break;
                 case 10:
-                        printf("%d-Octobre-%d", j,an);
+                        printf("%d-Octobre-%d", d,y);
                         break;
                 case 11:
-                        printf("%d-Novembre-%d", j,an);
+                        printf("%d-Novembre-%d", d,y);
                         break;
 
                 case 12:
-                        printf("%d-Decembre-%d", j,an);
+                        printf("%d-Decembre-%d", d,y);
                         break;
                 default:
                         break;
