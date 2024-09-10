afd9e78697ba06f8d255a064f0fcdaf0306d4864
ayoub <ayoubbenomar1230@gmail.com>
2024-09-10 15:07:14 +0100

    condition

--- SAS1/conditions/condition5.c
+++ SAS1/conditions/condition5.c
@@ -1,29 +1,42 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(){
+        int y;
+        printf("Entrer un nombre d'annee: ");
+        scanf("%d", &y);
+        getchar();
+        printf("\nPour convertir mois: entrer 1");
+        printf("\nPour convertir jours: entrer 2");
+        printf("\nPour convertir heures: entrer 3");
+        printf("\nPour convertir minutes: entrer 4");
+        printf("\nPour convertir second: entrer 5");
+        int x;
+        scanf("%d", &x);
+        int mois = y * 12;
+        int jours = y * 365;
+        int heures = y * 365 * 24;
+        int min = y * 365 * 24 * 60;
+        int seconds = y * 365 * 24 * 60*60;
+        switch(x){
+                case 1:
+                        printf("%d de annees est %d mois", y ,mois);
+                        break;
+                case 2:
+                        printf("%d de annees est %d jours", y ,jours);
+                        break;
+                case 3:
+                        printf("%d de annees est %d heures", y ,heures);
+                        break;
+                case 4:
+                        printf("%d de annees est %d minutes", y ,min);
+                        break;
+                case 5:
+                        printf("%d de annees est %d seconds", y ,seconds);
+                        break;
+                default:
+                        break;
+        }
+        return 0;
 
-    float y;
-    float r;
-    int annee;
-
-    printf("Entrer l'annee :");
-    scanf("%f", &y);
-    printf("Choisir : \n");
-    printf("1 ==> mois : \n");
-    printf("2 ==> jours : \n");
-    printf("3 ==> minutes : \n");
-    printf("4 ==> seconds : \n");
-
-    switch (annee)
-    {
-    case 1:
-       
-        break;
-    
-    default:
-        break;
-    }
-
-    
-    return 0;
 }
